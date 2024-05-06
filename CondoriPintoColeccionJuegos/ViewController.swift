7d9adfa9c752aefc4fb12fbac53d0bd9cae01a75
JJCondoriPinto <juanjosecondoripinto@gmail.com>
2024-05-06 13:27:03 -0500

    initial

---
+++ CondoriPintoColeccionJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CondoriPintoColeccionJuegos
+//
+//  Created by Neals Paye Aguilar on 6/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
