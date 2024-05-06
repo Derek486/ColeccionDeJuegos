d853996fd28e16eb2ecf6e920bcd4c016ad6759f
JJCondoriPinto <juanjosecondoripinto@gmail.com>
2024-05-06 14:18:44 -0500

    imagenes

---
+++ CondoriPintoColeccionJuegos/JuegosViewController.swift
@@ -0,0 +1,40 @@
+//
+//  JuegosViewController.swift
+//  CondoriPintoColeccionJuegos
+//
+//  Created by Neals Paye Aguilar on 6/05/24.
+//
+
+import UIKit
+
+class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+    @IBAction func camaraTapped(_ sender: Any) {
+    }
+    @IBAction func fotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let juego = Juego(context: context)
+        juego.titulo = tituloTextField.text
+        juego.imagen = imageView.image?.jpegData(compressionQuality: 0.50)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
+    }
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tituloTextField: UITextField!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let imagen = info[.originalImage] as? UIImage
+        imageView.image = imagen
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+}
